781560c336eeaea2703b1431eeb879493a278b12
Marcus <mdaygmdesigner@gmail.com>
2016-01-31 09:54:59 +1100

    Added more spells as prefabs
    Made there scripts

---
+++ Assets/Scripts/Spell Animations/MagicBomb.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MagicBomb : MonoBehaviour {
+
+	public int numberToFall;
+	public int turnNumb;
+
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if(turnNumb >= numberToFall)
+		{
+			//Drop, deal damage
+			Destroy(gameObject);
+		}
+	
+	}
+}
