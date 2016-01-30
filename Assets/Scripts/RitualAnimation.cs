e6b5fcbec8d472d63d45bc0e0a793ab8f80cd61c
Marcus <mdaygmdesigner@gmail.com>
2016-01-30 18:15:47 +1100

    More work ritual Animation Scripts

--- Assets/Scripts/RitualAnimation.cs
+++ Assets/Scripts/RitualAnimation.cs
@@ -87,6 +87,99 @@ public class RitualAnimation : MonoBehaviour
 		source.castingAnimation = false;
 	}
 
+	public void HealingWaterAnimation()
+	{
+		Player source = GameController.instance.currentCaster;
+		Player target = source.getTurn ().target;
+
+		source.castingAnimation = false;
+		
+	}
+
+	public void DisenchantAnimation()
+	{
+		Player source = GameController.instance.currentCaster;
+		Player target = source.getTurn ().target;
+
+		source.castingAnimation = false;
+
+	}
+
+	public void ConfuseAnimation()
+	{
+		Player source = GameController.instance.currentCaster;
+		Player target = source.getTurn ().target;
+
+		source.castingAnimation = false;
+
+	}
+
+	public void MirrorAnimation()
+	{
+		Player source = GameController.instance.currentCaster;
+		Player target = source.getTurn ().target;
+
+		source.castingAnimation = false;
+
+	}
+
+	public void MagicBombAnimation()
+	{
+		Player source = GameController.instance.currentCaster;
+		Player target = source.getTurn ().target;
+
+		source.castingAnimation = false;
+
+	}
+
+	public void FreezeAnimation()
+	{
+		Player source = GameController.instance.currentCaster;
+		Player target = source.getTurn ().target;
+
+		source.castingAnimation = false;
+
+	}
+
+	public void PowerSurgeAnimation()
+	{
+		Player source = GameController.instance.currentCaster;
+		Player target = source.getTurn ().target;
+
+		source.castingAnimation = false;
+
+	}
+
+	public void CompanionAnimation()
+	{
+		Player source = GameController.instance.currentCaster;
+		Player target = source.getTurn ().target;
+
+		source.castingAnimation = false;
+
+	}
+
+	public void FireCanonAnimation()
+	{
+		Player source = GameController.instance.currentCaster;
+		Player target = source.getTurn ().target;
+
+		source.castingAnimation = false;
+
+	}
+
+	public void DrainAnimation()
+	{
+		Player source = GameController.instance.currentCaster;
+		Player target = source.getTurn ().target;
+
+		source.castingAnimation = false;
+
+	}
+
+
+
+
 
 
 }
